a809a881ea95078eac8caa94fb062a41fba36b28
sebastian-p-c <seb.porma@duocuc.cl>
2024-09-11 20:04:19 -0300

    cambios realizados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  username: string = 'user';
+  password: string = '1234';
+  loginMessage!: string;
 
+  constructor(private router: Router) {}
+
+  Loginvalidate() {
+    console.log("Ejecutando validación");
+
+    if (this.username === 'admin' && this.password === 'admin') {
+      this.loginMessage = 'Login correcto';
+
+      let extras: NavigationExtras = {
+        state: { user: this.username }
+      }
+
+      this.router.navigate(['/index'], extras);
+
+    } else {
+      this.loginMessage = 'Login incorrecto';
+    }
+  }
 }
+
